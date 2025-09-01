82b330cb2bcf10e963c0b32f9b70b1ad8900f236
SaifSamir1 <saifsamirsaif12@gmail.com>
2025-09-01 11:03:19 +0300

    saif push

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:swd_app/session_coding/bottom_nav_screen.dart';
-import 'package:swd_app/task10/home_screen.dart';
 import 'package:swd_app/task11/splash_screen.dart';
 
 void main() {
@@ -12,6 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
   }
 }
